f2b6317013ade0f54afc1ba5828c1c207d1dcd3c
Jordan Suchow <suchow@post.harvard.edu>
2020-06-28 00:56:24 -0400

    Elementwise

--- src/ROC.jl
+++ src/ROC.jl
@@ -3,7 +3,7 @@ function AUC_ROC(Ytrue, scores)
      
      roc_y = Ytrue[perm]
      stack_x = cumsum(roc_y)/sum(roc_y)
-     stack_y = cumsum(! roc_y)/sum(! roc_y)    
+     stack_y = cumsum(.!roc_y)/sum(.!roc_y)
      N = length(roc_y)
      
      auc = sum( (stack_x[2:N] - stack_x[1:N-1]) .* stack_y[2:N] )
