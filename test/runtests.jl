236216074d160823efec03e98e15850343178bad
Jordan Suchow <suchow@post.harvard.edu>
2020-06-28 12:15:22 -0400

    Enable HMC test

--- test/runtests.jl
+++ test/runtests.jl
@@ -19,8 +19,8 @@ include("solver.jl")                      # ✓✓
 # include("sparsebin_csr.jl")               # ✗✓
 include("sbm.jl")                         # ✓✓
 include("parallel_latent_basic.jl")       # ✓✓
+include("macau_hmc.jl")                   # ✓✓
 #
-# include("macau_hmc.jl")                   # ✗✓
 
 # include("parallel_matrix.jl")             # ✗✗
 # include("parallel_mult.jl")               # ✗✗
