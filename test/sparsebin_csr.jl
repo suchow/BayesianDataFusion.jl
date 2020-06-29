0703bcc4842ce30caf141937ee99c6b9c9f2d7a4
Jordan Suchow <suchow@post.harvard.edu>
2020-06-28 20:34:43 -0400

    Specify which A_mul_B! to use

--- test/sparsebin_csr.jl
+++ test/sparsebin_csr.jl
@@ -11,12 +11,12 @@ csr = SparseBinMatrixCSR(rows, cols)
 y = zeros(Float64, size(csr, 1))
 x = rand(Float64,  size(csr, 2))
 
-A_mul_B!(y, csr, x)
+BayesianDataFusion.A_mul_B!(y, csr, x)
 
 ######### SparseBinMatrix  ############
 bin = SparseBinMatrix(rows, cols)
 y2 = zeros(Float64, size(bin, 1))
-A_mul_B!(y2, bin, x)
+BayesianDataFusion.A_mul_B!(y2, bin, x)
 
 A = sparse(rows, cols, 1.0)
 
